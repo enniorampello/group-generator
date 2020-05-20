04b8470f145fe5dcf7a20b203eef7353307ca936
enniorampello <enniorampello5@gmail.com>
2020-05-20 10:40:56 +0200

    second commit

--- main.py
+++ main.py
@@ -6,12 +6,42 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import ElementClickInterceptedException
 
 
+CHROME_PROFILE_PATH = '--user-data-dir=/Users/enniorampello/Library/Application Support/Google/Chrome'
+CHROME_DRIVER_PATH = '/Users/enniorampello/venv/chromedriver'
+TEXT_FILE_PATH = '/Users/enniorampello/GroupGenerator/group_names.txt'
+
+hamburger_button_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[1]/div/a'
+popup_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div'
+new_group_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[1]/div/ul/li[1]/a'
+search_tab_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/div[1]/input'
+riccardo_cuccu_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/div[2]/div/div[1]/ul/li/a/div[2]'
+create_group_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[3]/div/button[2]'
+group_name_tab_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[1]/form/div/input'
+confirm_create_group_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/button[2]'
+group_header_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a/div'
+popup_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div'
+upgrade_supergroup_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[3]/div[1]/div[5]/div/a'
+confirm_upgrade_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[2]/button[2]/span'
+group_upgraded_message = '//*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div/div/span/span/span'
+modify_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[1]/div[1]/div[1]/a[2]'
+description_tab_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[1]/form/div[2]/input'
+save_modification_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[2]/button[2]'
+
+
+def click(xpath):
+    wait.until(ec.element_to_be_clickable((By.XPATH, xpath))).click()
+
+def write(xpath: str, text: str):
+    wait.until(ec.presence_of_element_located((By.XPATH, xpath))).send_keys(text)
+
+
+
 # load the default profile in order not to login every time
 options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=/Users/enniorampello/Library/Application Support/Google/Chrome')
+options.add_argument(CHROME_PROFILE_PATH)
 
 # get the chromedriver and open Telegram already logged in
-driver = webdriver.Chrome(executable_path='/Users/enniorampello/venv/chromedriver', options=options)
+driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, options=options)
 driver.get('https://web.telegram.org/')
 
 # default object to wait until an element becomes clickable
@@ -20,7 +50,7 @@ wait = WebDriverWait(driver, timeout=30)
 time.sleep(4)
 
 # open the file containing in each line the name of a group
-file = open('/Users/enniorampello/GroupGenerator/group_names.txt', 'r')
+file = open(TEXT_FILE_PATH, 'r')
 lines = file.readlines()
 
 # iterate through all the group names and create the corresponding Telegram groups
@@ -29,72 +59,48 @@ for line in lines:
     name = line.split(',')[0]
     code = line.split(',')[1]
     # selecting the createGroup() button and click it
-    hamburger_button_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[1]/div/a'
     try:
-        wait.until(ec.element_to_be_clickable((By.XPATH,hamburger_button_xpath))).click()
+        click(hamburger_button_xpath)
     except ElementClickInterceptedException:
-        popup_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div'
         wait.until(ec.invisibility_of_element((By.XPATH, popup_xpath)))
-        wait.until(ec.element_to_be_clickable((By.XPATH, hamburger_button_xpath))).click()
+        click(hamburger_button_xpath)
 
-    new_group_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[1]/div/ul/li[1]/a'
-    wait.until(ec.element_to_be_clickable((By.XPATH,new_group_xpath))).click()
-
+    click(new_group_xpath)
 
     # select riccardo cuccu to create a group and click on create
-    search_tab_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/div[1]/input'
-    wait.until(ec.presence_of_element_located((By.XPATH,search_tab_xpath))).send_keys('riccardo cuccu')
-
-    riccardo_cuccu_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/div[2]/div/div[1]/ul/li/a/div[2]'
-    wait.until(ec.element_to_be_clickable((By.XPATH,riccardo_cuccu_xpath))).click()
-
-    create_group_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[3]/div/button[2]'
-    wait.until(ec.element_to_be_clickable((By.XPATH,create_group_xpath))).click()
+    write(search_tab_xpath,'riccardo cuccu')
+    click(riccardo_cuccu_xpath)
+    click(create_group_xpath)
 
     time.sleep(1)
     # write the name of the group and create it
-    group_name_tab_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[1]/form/div/input'
-    wait.until(ec.presence_of_element_located((By.XPATH,group_name_tab_xpath))).send_keys(name)
-
-    confirm_create_group_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[2]/button[2]'
-    wait.until(ec.element_to_be_clickable((By.XPATH,confirm_create_group_xpath))).click()
+    write(group_name_tab_xpath,name)
+    click(confirm_create_group_xpath)
 
     time.sleep(1)
     # add comment...
-    group_header_xpath = '//*[@id="ng-app"]/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a/div'
-    popup_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div'
     try:
-        wait.until(ec.element_to_be_clickable((By.XPATH, group_header_xpath))).click()
+        click(group_header_xpath)
     except ElementClickInterceptedException:
         time.sleep(1)
         wait.until(ec.invisibility_of_element((By.XPATH, popup_xpath)))
-        wait.until(ec.element_to_be_clickable((By.XPATH, group_header_xpath))).click()
+        click(group_header_xpath)
 
-    upgrade_supergroup_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[3]/div[1]/div[5]/div/a'
-    wait.until(ec.element_to_be_clickable((By.XPATH, upgrade_supergroup_xpath))).click()
-
-    confirm_upgrade_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[2]/button[2]/span'
-    wait.until(ec.element_to_be_clickable((By.XPATH, confirm_upgrade_xpath))).click()
+    click(upgrade_supergroup_xpath)
+    click(confirm_upgrade_xpath)
 
 
-    group_upgraded_message = '//*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/' \
-                             'div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div/div/span/span/span'
     wait.until(ec.presence_of_element_located((By.XPATH, group_upgraded_message))).click()
     try:
-        wait.until(ec.element_to_be_clickable((By.XPATH, group_header_xpath))).click()
+        click(group_header_xpath)
     except ElementClickInterceptedException:
         wait.until(ec.invisibility_of_element((By.XPATH, popup_xpath)))
-        wait.until(ec.element_to_be_clickable((By.XPATH, group_header_xpath))).click()
+        click(group_header_xpath)
 
 
-    modify_xpath = '//*[@id="ng-app"]/body/div[6]/div[2]/div/div/div[1]/div[1]/div[1]/a[2]'
-    wait.until(ec.element_to_be_clickable((By.XPATH, modify_xpath))).click()
-
-    description_tab_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[1]/form/div[2]/input'
+    click(modify_xpath)
     time.sleep(1)
-    wait.until(ec.presence_of_element_located((By.XPATH, description_tab_xpath))).send_keys(f'Group of the course: {name}; Code: {code};')
-
-    save_modification_xpath = '//*[@id="ng-app"]/body/div[7]/div[2]/div/div/div[2]/button[2]'
-    wait.until(ec.element_to_be_clickable((By.XPATH, save_modification_xpath))).click()
+    write(description_tab_xpath,f'Group of the course: {name}; Code: {code};')
+    click(save_modification_xpath)
 
 file.close()
