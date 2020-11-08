9e847f1955f916d6fc4701350923c4e41f951c0d
ennio <enniorampello5@gmail.com>
2020-11-08 16:46:09 +0100

    semi-definitive version of the algorithm added file scraper.py

--- main.py
+++ main.py
@@ -1,76 +1,40 @@
 from telethon import TelegramClient
 from telethon.tl import functions
-from telethon.client.chats import ChatMethods
-from telethon.events.newmessage import NewMessage
 import asyncio
-from time import sleep
+from keys import get_keys
 
-with open('./api.txt', 'r') as keys:
-    api_id = int(keys.readline())
-    api_hash = keys.readline()
 
-default_msg = 'Hi! This message has been sent (again) through the Telegram Api.'
-group_help_bot_username = 'GroupHelpBot'
-add_bot_command = '/start@GroupHelpOfficialClone3Bot'
+api_id = get_keys()['id']
+api_hash = get_keys()['hash']
 
+group_help_bot_username = 'GroupHelpOfficialClone2Bot'
+channel_ids = []
 
 async def main():
     async with TelegramClient('groupgen', api_id, api_hash) as client:
-        channel_name = 'third group'
+        channel_name = 'test channel'
         result = await client(functions.channels.CreateChannelRequest(
             title=channel_name,
             about='This group has been created through the Telegram API',
             megagroup=True,
         ))
         channel_id = result.updates[1].channel_id
-        await ChatMethods.edit_permissions(
-            client,
-            channel_id,
-            change_info=False,
-            pin_messages=False
-        )
-
-        await client.send_message(
-            channel_id,
-            add_bot_command
-        )
-
-        NewMessage(
-            channel_id,
-            incoming=True,
-            outgoing=False,
-            from_users=group_help_bot_username,
-            func= add_bot(client, channel_id)
-        )
-
-        await ChatMethods.edit_admin(
-            client,
+        channel_ids.append(channel_id)
+        result = await client(functions.channels.InviteToChannelRequest(
+            channel_id, 
+            [group_help_bot_username]
+            ))
+        await client.edit_admin(
             channel_id,
             group_help_bot_username,
-            is_admin=True
+            is_admin=True,
+            title='spam god'
         )
 
-        sleep(60)
-
-        await client(functions.channels.DeleteChannelRequest(
-            channel_id
-        ))
-
-async def add_bot(client, channel_id):
-    return await client.send_message(
-        channel_id,
-        add_bot_command
-    )
 
 if __name__ == '__main__':
     asyncio.run(main())
 
-
-
 '''
-third course,157684
-fourth course,321321
-fifth course,789456
-sixth course,654963
-seventh course,357951
+hour_count = 3g
 '''
